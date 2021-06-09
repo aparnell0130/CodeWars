88b5d75914377c59770e8e507ad307e253bdaa1f
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-06-08 22:32:00 -0700

    updated function to account for case

--- VowelCount/index.js
+++ VowelCount/index.js
@@ -5,10 +5,14 @@ function getCount(str) {
     const vowels = ['a', 'e', 'i', 'o', 'u']
 
     for (let i = 0; i < str.length; i++) {
-        if (vowels.includes(str[i])) {
+        if (vowels.includes(str[i].toLowerCase())) {
             vowelsCount++
         }
     }
 
     return vowelsCount;
 }
+
+getCount("abracadabra") //, 5)
+getCount("In The End") //, 3)
+getCount("Call Me Maybe") //, 4)
