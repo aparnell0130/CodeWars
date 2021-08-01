0ea2d110ac95712ad5bab32b3be9d829cf3d55c7
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-08-01 09:23:24 -0700

    added function to change background of individual light every second

--- TrafficLight/index.js
+++ TrafficLight/index.js
@@ -1,26 +1,46 @@
 const red = document.querySelector('.red')
 const yellow = document.querySelector('.yellow')
 const green = document.querySelector('.green')
-
+const colors = ['red', 'yellow', 'green']
+let i = 0
 function updateLight(current) {
     let newLight
     //your code here!
     if (current === 'green') {
         newLight = 'yellow'
         yellow.style.background = 'yellow'
+        green.style.background = 'black'
+        red.style.background = 'black'
+
     }
     if (current === 'yellow') {
         newLight = 'red'
         red.style.background = 'red'
+        yellow.style.background = 'black'
+        green.style.background = 'black'
     }
     if (current === 'red') {
         newLight = 'green'
+
         green.style.background = '#00FF7F'
+        yellow.style.background = 'black'
+        red.style.background = 'black'
+
     }
     return newLight
 }
 
-updateLight('yellow')
+function change() {
+    updateLight(colors[i]);
+    i++;
+
+    if (i > colors.length - 1) {
+        i = 0;
+    }
+}
+setInterval(change, 1000);
+
+
 // console.log(updateLight("green"))
 // console.log(updateLight("yellow"))
 // console.log(updateLight("red"))
