a562bb4fdcf44f066a47119276757581fc1c4381
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-08-01 09:02:13 -0700

    updated function to change individual light background color

--- TrafficLight/index.js
+++ TrafficLight/index.js
@@ -1,19 +1,26 @@
+const red = document.querySelector('.red')
+const yellow = document.querySelector('.yellow')
+const green = document.querySelector('.green')
+
 function updateLight(current) {
     let newLight
     //your code here!
     if (current === 'green') {
         newLight = 'yellow'
+        yellow.style.background = 'yellow'
     }
     if (current === 'yellow') {
         newLight = 'red'
+        red.style.background = 'red'
     }
     if (current === 'red') {
         newLight = 'green'
+        green.style.background = '#00FF7F'
     }
     return newLight
 }
 
-
-console.log(updateLight("green"))
-console.log(updateLight("yellow"))
-console.log(updateLight("red"))
+updateLight('yellow')
+// console.log(updateLight("green"))
+// console.log(updateLight("yellow"))
+// console.log(updateLight("red"))
