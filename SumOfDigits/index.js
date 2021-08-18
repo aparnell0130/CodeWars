2caf5578b1e241e4ba8fa830a731dc0a1ceaa5e8
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-08-18 13:49:25 -0700

    fixed issue had to add parseInt to newNum[i] cannot + string

--- SumOfDigits/index.js
+++ SumOfDigits/index.js
@@ -2,10 +2,16 @@ const digitalRoot = (n) => {
     let sum = 0
     let newNum = n.toString()
     for (let i = 0; i < newNum.length; i++) {
-        sum += newNum[i]
+        sum += parseInt(newNum[i])
     }
     if (sum < 10) {
         return sum
     }
     return digitalRoot(sum)
 }
+
+console.log(digitalRoot(16));
+console.log(digitalRoot(456));
+console.log(digitalRoot(942));
+console.log(digitalRoot(132189));
+console.log(digitalRoot(493193));
