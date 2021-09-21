84faecc5c68f51b111dca8e7317f0d2a0aad23f9
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-09-21 13:18:30 -0700

    added loop to sort arr then  make the arr a str

--- DescendingOrder/index.js
+++ DescendingOrder/index.js
@@ -10,6 +10,17 @@ const descendingOrderNM = (n) => {
     for (let i = 0; i < strN.length; i++) {
         arr[arr.length] = strN[i]
     }
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+                let temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+        }
+        newN += arr[i]
+    }
 }
 
 
